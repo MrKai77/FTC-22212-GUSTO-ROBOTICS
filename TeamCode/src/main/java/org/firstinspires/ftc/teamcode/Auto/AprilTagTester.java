17b6ea3d57cf8217cadcf5cc41b9502600af0e45
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2023-10-18 19:44:56 -0600

    ✨ Add `AprilTagTester`

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AprilTagTester.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Autonomous(name = "TESTER: April Tags")
+public class AprilTagTester extends  OpMode {
+    private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal visionPortal;
+
+    @Override
+    public void init() {
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+        visionPortal = VisionPortal.easyCreateWithDefaults(webcamName, aprilTagProcessor);
+    }
+
+    @Override
+    public void init_loop() {
+        List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
+        StringBuilder idsFound = new StringBuilder();
+
+        for (AprilTagDetection detection : currentDetections) {
+            idsFound.append(detection.id);
+            idsFound.append(", ");
+        }
+        telemetry.addData("April Tags", idsFound);
+    }
+
+    @Override
+    public void start() {
+        visionPortal.stopStreaming();
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/GourmetGustaaf.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/GourmetGustaaf.java
@@ -3,11 +3,9 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+@TeleOp(name = "Gourmet Gustaaf")
 public class GourmetGustaaf extends OpMode {
     private DcMotor liftMotor;
     private Servo planeLauncher;
