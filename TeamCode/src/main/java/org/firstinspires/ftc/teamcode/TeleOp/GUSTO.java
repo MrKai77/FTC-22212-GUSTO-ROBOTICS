5913d62bfae3a9c188b2c3919aab087d7d13b55d
Kai Azim <68963405+MrKai77@users.noreply.github.com>
2024-02-07 17:42:53 -0700

    ✨ Latest updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/GUSTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/GUSTO.java
@@ -45,24 +45,37 @@ public class GUSTO extends OpMode {
 
     @Override
     public void loop() {
-        double speedDivisor = gamepad.getButton(GamepadKeys.Button.LEFT_STICK_BUTTON) ? 1 : 2;
-        drive.m_drive.arcadeDrive(gamepad.getLeftY(), gamepad.getLeftX());
+        double speedDivisor = gamepad.getButton(GamepadKeys.Button.LEFT_STICK_BUTTON) ? 4 : 2;
+        drive.m_drive.arcadeDrive(gamepad.getLeftY() / speedDivisor, gamepad.getLeftX() / speedDivisor);
 
         if (gamepad1.x) {
             planeLauncher.liftoff();
         }
 
-        if (gamepad1.dpad_up) {
-            liftMotor.set(-0.7);
+        double position = -liftMotor.getCurrentPosition();
+
+        if (gamepad1.dpad_up && position <= 420) {
+            liftMotor.set(-0.6);
             clawServo.setPosition(0.25);
         } else if (gamepad1.dpad_down) {
-            liftMotor.set(0.6);
+            if (position <= 200) {
+                liftMotor.set(0.2);
+            } else {
+                liftMotor.set(0.5);
+            }
+
             clawServo.setPosition(0.25);
         } else {
             liftMotor.set(0);
 
             double clawTarget = gamepad.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) / 4;
             clawServo.setPosition(clawTarget);
+        }
+
+        telemetry.addData("Lift Motor", liftMotor.getCurrentPosition());
+
+        if (gamepad1.a) {
+            liftMotor.resetEncoder();
         }
 
         telemetry.update();
